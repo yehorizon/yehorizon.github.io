bd794888b378a6cb7bfd3d401d72cb594d38cf9a
yehorizon <75030577+yehorizon@users.noreply.github.com>
2024-03-15 23:34:03 +0300

    feat: add note

--- update.sh
+++ update.sh
@@ -1,8 +1,23 @@
 
 
-# Input message for commit
-echo "Enter message for commit: ";
-read message;
+# git submodule init;
+# git submodule update;
+
+echo "Updating _posts";
+git submodule update --remote --rebase _posts;
+# cd _posts; 
+# git fetch;
+# git merge;
+echo "Merged fetched _posts";
+
+# cd _posts;
+# git checkout main;
+# git submodule update --remote --merge _posts;
 
 
-echo $message;
+# # Input message for commit
+# echo "Enter message for commit: ";
+# read message;
+
+
+# echo $message;
